25689781cbbfe50d3066e74ef9f3987652dc15a4
saelric <saelric@gmail.com>
2023-08-02 11:47:42 -0600

    update

---
+++ source/php_files/alternative_php/example_php_alternative.php
@@ -0,0 +1,12 @@
+<?php
+
+$LANG[&#39;_MONDAY&#39;] = "Monday";
+$LANG["_TUESDAY"] = &#39;Tuesday&#39;;
+
+/**This is a multiline
+ * comment***/
+$LANG["_WEDNESDAY"] = &#39;&#39;;
+
+$LANG["_Thursday"] = "Thursday";
+
+?>
---
+++ source/php_files/array_php/example_php_array.php
@@ -0,0 +1,15 @@
+<?php
+$LANG = array(
+    "january"   => "enero",
+    "february"  => "febrero",
+    "march"     => "marzo",
+    "april"     => "abril",
+    "may"       => "mayo",
+    "june"      => "junio",
+    "july"      => "julio",
+    "august"    => "agosto",
+    "september" => "septiembre",
+    "october"   => "octubre",
+    "november"  => "noviembre",
+    "december"  => "diciembre"
+);
